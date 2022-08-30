5c03843c32a901ad3c5c6043b8daeccf0b88a582
manuinfan <manu.infantino1@gmail.com>
2022-08-30 20:23:11 -0300

    Clase 4

---
+++ JS/app.js
@@ -0,0 +1,4 @@
+let añoActual = (prompt("Ingrese el año actual"))
+let añoNacimiento = (prompt("Ingrese su año de nacimiento"))
+let resultado = añoActual - añoNacimiento;
+console.log("Usted tiene" + " " + resultado + " " + "años")
